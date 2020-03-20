3a5de87044c6c43e10fe2bba15c02c011d676c2c
rage73 <raghavagrawal2000ra@gmail.com>
2020-03-20 23:15:50 +0530

    a2oj l11

--- perfectpermutation.cpp
+++ perfectpermutation.cpp
@@ -2,8 +2,28 @@
 using namespace std;
 // https://codeforces.com/problemset/problem/233/A
 
+#define ll			long long
+#define pb			push_back
+#define setbits(x)	__builtin_popcountll(x)
+#define MOD			1000000007
+
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     
+    int n;
+    cin>>n;
+    if(!(n%2))
+    {
+        for(int i=n;i>=1;i--)
+        {
+            cout<<i<<" ";
+        }
+    }
+    else
+    {
+        cout<<"-1";
+    }
     return 0;
 }
